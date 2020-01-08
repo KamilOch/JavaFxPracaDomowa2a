e6e2e76d8e3b0c90559a8de70798b70464eed898
Kamil Ochnik <ilmak@o2.pl>
2020-01-08 14:08:16 +0100

    added 25 clippedImages

--- src/pracaDomowa2a/MainPracaDomowa2a.java
+++ src/pracaDomowa2a/MainPracaDomowa2a.java
@@ -6,6 +6,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -14,6 +15,9 @@ import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Stop;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainPracaDomowa2a extends Application {
@@ -34,23 +38,46 @@ public class MainPracaDomowa2a extends Application {
 
             Label text = new Label("Wycinki w kolejności malejącej średniej wartości składowej czerwonej.");
 
-            Rectangle clippedImage = new Rectangle(100, 200, 41, 41);
-            clippedImage.setStroke(Color.BLACK);
-            clippedImage.setFill(Color.WHITE);
-            clippedImage.setStrokeWidth(3);
+            GridPane clippedImagesGrid = new GridPane();
+            clippedImagesGrid.addColumn(5);
+            clippedImagesGrid.addRow(5);
+            clippedImagesGrid.setHgap(20);
+            clippedImagesGrid.setVgap(20);
+
+            List<Rectangle> clippedImages = new ArrayList<>();
+
+            for (int i = 0; i <= 25; i++) {
+
+                Rectangle clippedImage = new Rectangle(41, 41);
+                clippedImage.setStroke(Color.BLACK);
+                clippedImage.setFill(Color.WHITE);
+                clippedImage.setStrokeWidth(3);
+
+                clippedImages.add(clippedImage);
+            }
+
+            int y = 0;
+            for (int i = 0; i<5;i++) {
+                clippedImagesGrid.add(clippedImages.get(y), 0, i);
+                clippedImagesGrid.add(clippedImages.get(1+y), 1, i);
+                clippedImagesGrid.add(clippedImages.get(2+y), 2, i);
+                clippedImagesGrid.add(clippedImages.get(3+y), 3, i);
+                clippedImagesGrid.add(clippedImages.get(4+y), 4, i);
+                y = y +5;
+            }
 
             VBox buttonsLanelBox = new VBox();
-            buttonsLanelBox.getChildren().addAll(buttonBox, text, clippedImage);
+            buttonsLanelBox.getChildren().addAll(buttonBox, text, clippedImagesGrid);
 
             Rectangle mainPicture = new Rectangle(50, 50, 500, 700);
             mainPicture.setStroke(Color.BLACK);
             mainPicture.setStrokeWidth(4);
             mainPicture.setFill(Color.WHITE);
 
-            LinearGradient linearGradient = new LinearGradient(0.7,0,1,0.7,true, CycleMethod.REFLECT,
-                    new Stop(0,Color.BLUE),
-                    new Stop(0.5,Color.RED),
-                    new Stop(1,Color.GREEN)
+            LinearGradient linearGradient = new LinearGradient(0.7, 0, 1, 0.7, true, CycleMethod.REFLECT,
+                    new Stop(0, Color.BLUE),
+                    new Stop(0.5, Color.RED),
+                    new Stop(1, Color.GREEN)
             );
             mainPicture.setFill(linearGradient);
 
